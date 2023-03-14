f90af01b65a4ad4d370816eca160898f9c0a5f6d
kendra jarmon <kendra65239@hotmail.com>
2023-03-14 15:30:31 -0500

    add code

---
+++ cars.sql
@@ -0,0 +1,7 @@
+create database flaskrestapi_py_starter;
+
+INSERT INTO flaskrestapi_py_starter.car (make, model, year) VALUES ('Hunda', 'Civic', '2020');
+INSERT INTO flaskrestapi_py_starter.car (make, model, year) VALUES ('Chevrolet', 'Colorado', '2022');
+INSERT INTO flaskrestapi_py_starter.car (make, model, year) VALUES ('Mercedes', 'Benz', '2021');
+INSERT INTO flaskrestapi_py_starter.car (make, model, year) VALUES ('Acura', 'Integra', '2023');
+INSERT INTO flaskrestapi_py_starter.car (make, model, year) VALUES ('Ford', 'Maverick', '2022');
